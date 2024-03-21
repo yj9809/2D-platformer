48e00e86818aefc149cb48a34c55aeb4240d92af
yj9809 <147613573+yj9809@users.noreply.github.com>
2024-03-21 19:24:48 +0900

    몬스터 이동 로직 변경

--- Assets/1.Script/Enemy.cs
+++ Assets/1.Script/Enemy.cs
@@ -6,8 +6,6 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private int direction;
 
-    private Vector2 rayPos;
-    private RaycastHit2D[] ray = new RaycastHit2D[2];
     private Rigidbody2D rigid;
     private SpriteRenderer sprite;
 
@@ -27,10 +25,7 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         Move();
-        rayPos = new Vector2(transform.position.x, transform.position.y + 1);
-        Debug.Log(ray[0].collider.name);
-        ray[0] = Physics2D.Raycast(rayPos, Vector2.right, 1f, LayerMask.GetMask("Wall"));
-        ray[1] = Physics2D.Raycast(rayPos, Vector2.left, 1f, LayerMask.GetMask("Wall"));
+        Ground();
     }
     void Move()
     {
@@ -43,14 +38,7 @@ public class Enemy : MonoBehaviour
         {
             sprite.flipX = false;
         }
-
-        if (ray[0])
-            direction = -1;
-        else if (ray[1])
-            direction = 1;
-        else
         
-
         rigid.velocity = new Vector2(direction, rigid.velocity.y) * speed;
     }
     void ChangeDirection()
@@ -59,6 +47,30 @@ public class Enemy : MonoBehaviour
 
         Invoke("ChangeDirection", 2f);
     }
+
+    void Ground()
+    {
+        //Ground Check
+        Vector2 frontVec = new Vector2(rigid.position.x + (direction * 0.2f), rigid.position.y + 1);
+
+        RaycastHit2D rayHitDown = Physics2D.Raycast(frontVec, Vector2.down, 1f, LayerMask.GetMask("Ground"));
+
+        if (rayHitDown.collider == null)
+            direction *= -1;
+
+        //Wall Check
+        Vector2 rayDircetion = Vector2.left;
+
+        if (direction < 0)
+            rayDircetion = Vector2.left;
+        else if (direction > 0)
+            rayDircetion = Vector2.right;
+
+        RaycastHit2D rayHitWall = Physics2D.Raycast(frontVec, rayDircetion, 1f, LayerMask.GetMask("Wall"));
+        if (rayHitWall.collider != null)
+            direction *= -1;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Player>())
